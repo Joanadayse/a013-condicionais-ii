7aa184d68e7f5698d4efd8d267e41f67e92f58a6
Joanadayse <daysejoana3@gmail.com>
2022-08-09 20:40:08 -0300

    exercicio1-corrigido

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -2,19 +2,23 @@ const num = Number(prompt("digite um numero"))
  
 if (num % 2 === 0) {
     if (num % 3 === 0) {
-        console.log ("esse numero  é divisivel por 2 e por 3 " ) }
-    
-} else {
-    console.log ("esse numero  não é divisivel por 2 e por 3 ")
-
-
+        console.log ("esse numero  é divisivel por 2 ou por 3 " ) }
+    else if (!(num %3===0)){
+        console.log ("esse numero  é divisivel por 2 ou por 3 " )}
+} else if (!(num % 2 === 0)){
+    if (num % 3 === 0) {
+        console.log ("esse numero  é divisivel por 2 ou por 3 " ) }
+        else {
+        console.log ("esse numero  não é divisivel por 2 e por 3 ")
+} 
 }
 
-if(num % 2 === 0 || num % 3 === 0 ){ 
-    console.log("este numero é divisel por 2")
+
+if(num % 2 === 0 && num % 3 === 0 ){ 
+    console.log("este numero é divisel por 2 e por 3")
  
 } else {
-    console.log ("este numero é divisel por 3")
+    console.log ("este numero não é divisel por 3")
 } 
 
     
